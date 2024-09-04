990e4917556ed3179de86267c9e9267a8a31c5b4
MM-2103 <mohsen.menem@protonmail.com>
2024-09-04 16:08:21 +0200

    Added the restart patch and a keybinding

--- config.h
+++ config.h
@@ -75,6 +75,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
+static const char *closecmd[] = { "kill -HUP dwm", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -111,6 +112,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {.v = closecmd } }, 
 };
 
 /* button definitions */
